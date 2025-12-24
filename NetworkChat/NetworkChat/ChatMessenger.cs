67f8b296f0305ba556a4a6d0b76b27134c359716
alexmagikov <alexmagikov228@gmail.com>
2025-12-24 12:43:02 +0300

    feat: add all classes without tests

---
+++ NetworkChat/NetworkChat/ChatMessenger.cs
@@ -0,0 +1,64 @@
+﻿// <copyright file="ChatMessenger.cs" company="Alexander Kuchin">
+// Copyright (c) Alexander Kuchin. All rights reserved.
+// </copyright>
+
+namespace NetworkChat;
+
+using System.Net.Sockets;
+
+public class ChatMessenger(TextReader input, TextWriter output)
+{
+    public async Task RunAsync(NetworkStream stream, CancellationToken cts)
+    {
+        using var streamReader = new StreamReader(stream, leaveOpen: true);
+        using var streamWriter = new StreamWriter(stream, leaveOpen: true);
+        streamWriter.AutoFlush = true;
+
+        var read = ReadFromNetworkAsync(streamReader, cts);
+        var write = WriteToNetworkAsync(streamWriter, cts);
+        await Task.WhenAny(read, write);
+    }
+
+    private async Task WriteToNetworkAsync(StreamWriter writer, CancellationToken cts)
+    {
+        await output.WriteAsync("Local: ");
+        while (!cts.IsCancellationRequested)
+        {
+            var line = await Task.Run(input.ReadLine, cts);
+
+            if (line is null or "exit")
+            {
+                break;
+            }
+
+            await writer.WriteLineAsync(line.AsMemory(), cts);
+            await output.WriteAsync("Local: ");
+        }
+    }
+
+    private async Task ReadFromNetworkAsync(StreamReader reader, CancellationToken cts)
+    {
+        try
+        {
+            while (!cts.IsCancellationRequested)
+            {
+                var line = await reader.ReadLineAsync(cts);
+                if (line == null)
+                {
+                    break;
+                }
+
+                await output.WriteLineAsync($"Remote: {line}");
+                await output.WriteAsync("Local: ");
+            }
+        }
+        catch (IOException)
+        {
+            // ignored
+        }
+        catch (OperationCanceledException)
+        {
+            // ignored
+        }
+    }
+}
